ee846e0710aad53dea27e37ba17b2f84d70d9e5a
Agung Sugiarto <sugiartoagung92@gmail.com>
2022-08-22 01:34:11 +0700

    WIP: Legacy to laravel

--- src/Core/CI_Input.php
+++ src/Core/CI_Input.php
@@ -15,9 +15,18 @@ namespace Fluent\Legacy\Core;
 
 use Config\Services;
 use Fluent\Legacy\Exception\NotSupportedException;
+use Illuminate\Support\Facades\Cookie;
 
 class CI_Input
 {
+    /** @var \Illuminate\Http\Request */
+    protected $request;
+
+    public function __construct()
+    {
+        $this->request = request();
+    }
+
     /**
      * Fetch an item from the GET array
      *
@@ -30,9 +39,7 @@ class CI_Input
     {
         $this->checkXssClean($xss_clean);
 
-        $request = Services::request();
-
-        return $request->getGet($index);
+        return $this->request->get($index);
     }
 
     /**
@@ -47,20 +54,47 @@ class CI_Input
     {
         $this->checkXssClean($xss_clean);
 
-        $request = Services::request();
-
-        return $request->getPost($index);
+        return $this->request->post($index);
     }
 
-    private function checkXssClean(bool $xss_clean)
+    /**
+     * Fetch an item from POST data with fallback to GET
+     *
+     * @param   string  $index      Index for item to be fetched from $_POST or $_GET
+     * @param   bool    $xss_clean  Whether to apply XSS filtering
+     * @return  mixed
+     */
+    public function post_get($index, $xss_clean = false)
     {
-        if ($xss_clean !== false) {
-            throw new NotSupportedException(
-                '$xss_clean is not supported.'
-                .' Preventing XSS should be performed on output, not input!'
-                .' Use esc() <https://codeigniter4.github.io/CodeIgniter4/general/common_functions.html#esc> instead.'
-            );
-        }
+        return isset($_POST[$index])
+            ? $this->post($index, $xss_clean)
+            : $this->get($index, $xss_clean);
+    }
+
+    /**
+     * Fetch an item from GET data with fallback to POST
+     *
+     * @param   string  $index      Index for item to be fetched from $_GET or $_POST
+     * @param   bool    $xss_clean  Whether to apply XSS filtering
+     * @return  mixed
+     */
+    public function get_post($index, $xss_clean = false)
+    {
+        return isset($_GET[$index])
+            ? $this->get($index, $xss_clean)
+            : $this->post($index, $xss_clean);
+    }
+
+    /**
+     * Fetch an item from the COOKIE array
+     *
+     * @param   mixed   $index      Index for item to be fetched from $_COOKIE
+     * @param   bool    $xss_clean  Whether to apply XSS filtering
+     * @return  mixed
+     */
+    public function cookie($index = null, $xss_clean = false)
+    {
+        return $this->request->cookie($index);
     }
 
     /**
@@ -75,8 +109,170 @@ class CI_Input
     {
         $this->checkXssClean($xss_clean);
 
-        $request = Services::request();
+        return $this->request->server($index);
+    }
 
-        return $request->getServer($index);
+    /**
+     * Set cookie
+     *
+     * Accepts an arbitrary number of parameters (up to 7) or an associative
+     * array in the first parameter containing all the values.
+     *
+     * @param	string|mixed[]	$name		Cookie name or an array containing parameters
+     * @param	string		$value		Cookie value
+     * @param	int		$expire		Cookie expiration time in seconds
+     * @param	string		$domain		Cookie domain (e.g.: '.yourdomain.com')
+     * @param	string		$path		Cookie path (default: '/')
+     * @param	string		$prefix		Cookie name prefix
+     * @param	bool		$secure		Whether to only transfer cookies via SSL
+     * @param	bool		$httponly	Whether to only makes the cookie accessible via HTTP (no javascript)
+     * @param	string		$samesite	SameSite attribute
+     * @return	void
+     */
+    public function set_cookie($name, $value = '', $expire = '', $domain = '', $path = '/', $prefix = '', $secure = NULL, $httponly = NULL, $samesite = NULL)
+    {
+        return Cookie::queue(...func_get_args());
+    }
+
+    /**
+     * Fetch the IP Address
+     *
+     * Determines and validates the visitor's IP address.
+     *
+     * @return	string	IP address
+     */
+    public function ip_address()
+    {
+        return $this->request->ip();
+    }
+
+    /**
+     * Validate IP Address
+     *
+     * @param	string	$ip	IP address
+     * @param	string	$which	IP protocol: 'ipv4' or 'ipv6'
+     * @return	bool
+     */
+    public function valid_ip($ip, $which = '')
+    {
+        switch (strtolower($which)) {
+            case 'ipv4':
+                $which = FILTER_FLAG_IPV4;
+                break;
+            case 'ipv6':
+                $which = FILTER_FLAG_IPV6;
+                break;
+            default:
+                $which = 0;
+                break;
+        }
+
+        return (bool) filter_var($ip, FILTER_VALIDATE_IP, $which);
+    }
+
+    /**
+     * Fetch User Agent string
+     *
+     * @return	string|null	User Agent string or NULL if it doesn't exist
+     */
+    public function user_agent($xss_clean = false)
+    {
+        $this->checkXssClean($xss_clean);
+
+        return $this->request->userAgent();
+    }
+
+    /**
+     * Request Headers
+     *
+     * @param	bool	$xss_clean	Whether to apply XSS filtering
+     * @return	array
+     */
+    public function request_headers($xss_clean = false)
+    {
+        $this->checkXssClean($xss_clean);
+
+        return $this->request->header();
+    }
+
+    /**
+     * Get Request Header
+     *
+     * Returns the value of a single member of the headers class member
+     *
+     * @param	string		$index		Header name
+     * @param	bool		$xss_clean	Whether to apply XSS filtering
+     * @return	string|null	The requested header on success or NULL on failure
+     */
+    public function get_request_header($index, $xss_clean = false)
+    {
+        $this->checkXssClean($xss_clean);
+
+        return $this->request->header($index);
+    }
+
+    /**
+     * Is AJAX request?
+     *
+     * Test to see if a request contains the HTTP_X_REQUESTED_WITH header.
+     *
+     * @return 	bool
+     */
+    public function is_ajax_request()
+    {
+        $this->request->ajax();
+    }
+
+    /**
+     * Is CLI request?
+     *
+     * Test to see if a request was made from the command line.
+     *
+     * @deprecated	3.0.0	Use is_cli() instead
+     * @return	bool
+     */
+    public function is_cli_request()
+    {
+        return is_cli();
+    }
+
+    /**
+     * Get Request Method
+     *
+     * Return the request method
+     *
+     * @param	bool	$upper	Whether to return in upper or lower case
+     *				(default: FALSE)
+     * @return 	string
+     */
+    public function method($upper = false)
+    {
+        return ($upper)
+            ? strtoupper($this->request->method())
+            : strtolower($this->request->method());
+    }
+
+    /**
+     * Magic __get()
+     *
+     * Allows read access to protected properties
+     *
+     * @param	string	$name
+     * @return	mixed
+     */
+    public function __get($name)
+    {
+        return $this->request->__get($name);
+    }
+
+    private function checkXssClean(bool $xss_clean)
+    {
+        if ($xss_clean !== false) {
+            throw new NotSupportedException(
+                '$xss_clean is not supported.'
+                    . ' Preventing XSS should be performed on output, not input!'
+                    . ' Use esc() instead.'
+            );
+        }
     }
 }
